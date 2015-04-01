1bcad95f29c827ee7e4012a18308eefb9cd1fcec
Dave Herman <dherman@mozilla.com>
2015-04-01 15:34:55 -0700

    trying to JSON-decode test structs, but Json doesn't support RustcDecodable? src/test/mod.rs:12:10: 12:24 error: the trait `rustc_serialize::serialize::Decodable` is not implemented for the type `rustc_serialize::json::Json` [E0277] src/test/mod.rs:12 #[derive(RustcDecodable)]                             ^~~~~~~~~~~~~~ src/test/mod.rs:12:10: 12:24 note: in expansion of #[derive_RustcDecodable] src/test/mod.rs:12:10: 12:24 note: expansion site src/test/mod.rs:12:10: 12:24 error: the trait `rustc_serialize::serialize::Decodable` is not implemented for the type `rustc_serialize::json::Json` [E0277] src/test/mod.rs:12 #[derive(RustcDecodable)]                             ^~~~~~~~~~~~~~ src/test/mod.rs:12:10: 12:24 note: in expansion of #[derive_RustcDecodable] src/test/mod.rs:12:10: 12:24 note: expansion site error: aborting due to 2 previous errors

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,1 +1,57 @@
+#![cfg(test)]
+
+use std::collections::HashMap;
+use rustc_serialize::json;
+use rustc_serialize::json::{Json, Object};
+use rustc_serialize::{Decoder, Decodable};
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::Result;
+use token::{Token, ReservedWord};
+
+#[derive(RustcDecodable)]
+pub struct ExpectPass {
+    source: String,
+    expected: Json,
+    options: Option<Json>
+}
+
+pub struct ExpectFail {
+    source: String,
+    error: String,
+    options: Option<Json>
+}
+
+pub enum TestCase {
+    Pass(ExpectPass),
+    Fail(ExpectFail)
+}
+
+trait JsonExt {
+    fn into_array(self) -> json::Array;
+    fn into_string(self) -> String;
+    fn into_string_opt(self) -> Option<String>;
+}
+
+impl JsonExt for Json {
+    fn into_array(self) -> json::Array {
+        match self {
+            Json::Array(array) => array,
+            _ => panic!("expected array")
+        }
+    }
+    fn into_string(self) -> String {
+        match self {
+            Json::String(string) => string,
+            _ => panic!("expected string")
+        }
+    }
+    fn into_string_opt(self) -> Option<String> {
+        match self {
+            Json::Null => None,
+            Json::String(string) => Some(string),
+            _ => panic!("expected string or null")
+        }
+    }
+}
 
