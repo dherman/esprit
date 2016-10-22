0113ad1992336cf606a1fb2bf49e46e3d1886256
Ingvar Stepanyan <me@rreverser.com>
2016-10-22 15:38:11 +0100

    Speed up & fix test generation.
    1) Replaced js-beautify with serialize-js so that data is serialized once instead of separate reformatting step.
    
    Timings before:
    build-integration.js: 4.12s
    build-unit.js: 0.45s
    
    Timings after:
    build-integration.js: 0.78s
    build-unit.js: 0.17s
    
    2) Execute npm install just once for `make all`.
    
    3) Replaced console.log+redirection with direct writeFileSync as pure redirect results in UTF-16 file on Windows which Rust complains about.

--- tests/build-unit.js
+++ tests/build-unit.js
@@ -1,24 +1,15 @@
 var parse = require('esprima').parse;
-var beautify = require('js-beautify').js_beautify;
 var readFileSync = require('fs').readFileSync;
+var write = require('./write');
 
 var inputFile = process.argv[2];
+var outputFile = process.argv[3];
 
 var data = JSON.parse(readFileSync(inputFile, "utf8"));
 
-var results = [];
-
-for (var source in data) {
-  results.push({ source: source, expected: data[source] ? parse(source) : null });
-}
-
-// JSON.stringify creates a string literal with "\u2028" or "\u2029" in it,
-// which appears to choke the Rust JSON parser, so escape it.
-function stringify(x) {
-  return JSON.stringify(x)
-             .replace(/\u2028|\u2029/g, function(m) {
-               return "\\u202" + (m === "\u2028" ? "8" : "9");
-             });
-}
-
-console.log(beautify(stringify(results), { indent_size: 2 }));
+write(outputFile, Object.keys(data).map(function(source) {
+  return {
+    source: source,
+    expected: data[source] ? parse(source) : null
+  };
+}));
