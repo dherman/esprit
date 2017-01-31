0827fe5f1a6fd84c75d9370856cf1500ffb026d9
Ingvar Stepanyan <me@rreverser.com>
2017-01-31 21:19:04 +0000

    Use native custom derives by default
    Rust 1.15 has stable custom derives

--- crates/easter/src/lib.rs
+++ crates/easter/src/lib.rs
@@ -1,28 +1,15 @@
 extern crate joker;
 extern crate tristate;
 
-#[cfg(feature = "nightly")]
 #[macro_use]
 extern crate derive;
 
-#[cfg(feature = "nightly")]
-macro_rules! pub_mod {
-    ($name:ident) => (pub mod $name;)
-}
-
-#[cfg(not(feature = "nightly"))]
-macro_rules! pub_mod {
-    ($name:ident) => (pub mod $name {
-        include!(concat!(env!("OUT_DIR"), "/", stringify!($name), ".rs"));
-    })
-}
-
-pub_mod!(id);
-pub_mod!(fun);
-pub_mod!(obj);
-pub_mod!(stmt);
-pub_mod!(expr);
-pub_mod!(decl);
-pub_mod!(patt);
-pub_mod!(punc);
-pub_mod!(cover);
+pub mod id;
+pub mod fun;
+pub mod obj;
+pub mod stmt;
+pub mod expr;
+pub mod decl;
+pub mod patt;
+pub mod punc;
+pub mod cover;
